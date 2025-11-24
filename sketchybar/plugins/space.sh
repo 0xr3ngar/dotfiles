5b6255a7cfea9bd5d1629a0dea57e5f9789f245c
0xr3ngar <84232456+0xr3ngar@users.noreply.github.com>
2025-11-25 00:53:23 +0100

    chore: refresh local configuration files

--- sketchybar/plugins/space.sh
+++ sketchybar/plugins/space.sh
@@ -1,8 +1,17 @@
+# #!/bin/bash
+# if [ "$SELECTED" = "true" ]; then
+#   sketchybar --set "$NAME" background.drawing=on \
+#                           label.color=0xff000000    # Black text on bright blue
+# else
+#   sketchybar --set "$NAME" background.drawing=off \
+#                           label.color=0xff00aeff    # Bright blue text when inactive
+# fi
 #!/bin/bash
+
 if [ "$SELECTED" = "true" ]; then
-  sketchybar --set "$NAME" background.drawing=on \
-                          label.color=0xff000000    # Black text on bright blue
+    sketchybar --set $NAME background.drawing=on \
+                          icon.color=0xff1e2326
 else
-  sketchybar --set "$NAME" background.drawing=off \
-                          label.color=0xff00aeff    # Bright blue text when inactive
+    sketchybar --set $NAME background.drawing=off \
+                          icon.color=0xffd3c6aa
 fi
