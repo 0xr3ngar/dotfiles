d99fd92f17e885fca0ee4066bb3ba1ebdb2c2114
0xr3ngar <contact@bnn.dev>
2026-02-06 01:01:53 +0100

    feat(nvim): add plugin configuration file

--- nvim/lua/plugins/99.lua
+++ nvim/lua/plugins/99.lua
@@ -4,7 +4,6 @@ _99.setup({
     md_files = {
         "AGENT.md",
     },
-    model = "opencode/big-pickle",
     completion = {
         source = "cmp",
     },
@@ -25,3 +24,23 @@ end, { desc = "99: Stop all requests" })
 vim.keymap.set("n", "<leader>am", function()
     _99.select_model()
 end, { desc = "99: Select model" })
+
+vim.keymap.set("n", "<leader>ar", function()
+    _99.refactor_function()
+end, { desc = "99: Refactor function" })
+
+vim.keymap.set("n", "<leader>ae", function()
+    _99.explain_function()
+end, { desc = "99: Explain function" })
+
+vim.keymap.set("n", "<leader>aE", function()
+    _99.explain_function_prompt()
+end, { desc = "99: Explain function with prompt" })
+
+vim.keymap.set("n", "<leader>aes", function()
+    _99.show_explanation_window()
+end, { desc = "99: Show explanation in window" })
+
+vim.keymap.set("n", "<leader>ac", function()
+    _99.clear_explanations()
+end, { desc = "99: Clear explanations" })
