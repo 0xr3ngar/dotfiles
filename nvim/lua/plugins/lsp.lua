0aeec4dfe3ae567a22a25c7df0f43bcf20fb5c79
0xr3ngar <84232456+0xr3ngar@users.noreply.github.com>
2026-05-18 15:15:11 +0200

    chore: update terminal and neovim config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,11 +1,17 @@
 require("mason").setup()
 
 local lspconfig = require("lspconfig")
+local util = require("lspconfig.util")
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 lspconfig.lua_ls.setup({ capabilities = capabilities })
 lspconfig.ts_ls.setup({ capabilities = capabilities })
 lspconfig.eslint.setup({ capabilities = capabilities })
 lspconfig.clangd.setup({ capabilities = capabilities })
+lspconfig.pylsp.setup({
+    capabilities = capabilities,
+    root_dir = util.root_pattern("pyproject.toml", "setup.py", "setup.cfg", "requirements.txt"),
+    cmd = { "uv", "run", "--with", "python-lsp-server", "pylsp" },
+})
 
 vim.keymap.set("n", "pd", "<CMD>Glance implementations<CR>")
