783fb9a843aabf65da5fc393b072a4a16ba32ce3
0xr3ngar <84232456+0xr3ngar@users.noreply.github.com>
2026-06-01 01:11:13 +0200

    feat(nvim): let lsp speak the new tongue

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,17 +1,33 @@
 require("mason").setup()
 
-local lspconfig = require("lspconfig")
-local util = require("lspconfig.util")
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-lspconfig.lua_ls.setup({ capabilities = capabilities })
-lspconfig.ts_ls.setup({ capabilities = capabilities })
-lspconfig.eslint.setup({ capabilities = capabilities })
-lspconfig.clangd.setup({ capabilities = capabilities })
-lspconfig.pylsp.setup({
+-- Define LSP configurations using the new vim.lsp API (Neovim 0.11+)
+vim.lsp.config('lua_ls', {
     capabilities = capabilities,
-    root_dir = util.root_pattern("pyproject.toml", "setup.py", "setup.cfg", "requirements.txt"),
-    cmd = { "uv", "run", "--with", "python-lsp-server", "pylsp" },
 })
+
+vim.lsp.config('ts_ls', {
+    capabilities = capabilities,
+})
+
+vim.lsp.config('eslint', {
+    capabilities = capabilities,
+})
+
+vim.lsp.config('clangd', {
+    capabilities = capabilities,
+})
+
+vim.lsp.config('pylsp', {
+    capabilities = capabilities,
+    cmd = { "uv", "run", "--with", "python-lsp-server", "pylsp" },
+    root_dir = function(fname)
+        return vim.fs.root(fname, { "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt" })
+    end,
+})
+
+-- Enable the servers
+vim.lsp.enable({ 'lua_ls', 'ts_ls', 'eslint', 'clangd', 'pylsp' })
 
 vim.keymap.set("n", "pd", "<CMD>Glance implementations<CR>")
